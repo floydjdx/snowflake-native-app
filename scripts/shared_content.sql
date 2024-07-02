c207017d5bc71be894a3ed53865ff6af6934c5a2
Jordan Floyd <floyd.d.jordan@gmail.com>
2024-07-02 08:31:36 -0400

    Add streamlit app

--- scripts/shared_content.sql
+++ scripts/shared_content.sql
@@ -12,11 +12,3 @@ INSERT INTO accounts VALUES
 -- grant usage on the ``ACCOUNTS`` table
 GRANT USAGE ON SCHEMA shared_data TO SHARE IN APPLICATION PACKAGE {{package_name}};
 GRANT SELECT ON TABLE accounts TO SHARE IN APPLICATION PACKAGE {{package_name}};
-
-CREATE OR ALTER VERSIONED SCHEMA code_schema;
-GRANT USAGE ON SCHEMA code_schema TO APPLICATION ROLE app_public;
-
-CREATE VIEW IF NOT EXISTS code_schema.accounts_view
-  AS SELECT ID, NAME, VALUE
-  FROM shared_data.accounts;
-GRANT SELECT ON VIEW code_schema.accounts_view TO APPLICATION ROLE app_public;
